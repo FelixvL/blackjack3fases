6d4a89c3b75e78904af3ae91db5cc382507b97bd
FelixvL <digitaleservice@gmail.com>
2023-02-01 15:58:00 +0100

    boerderij voorbeeld met boeren

--- Demo.java
+++ Demo.java
@@ -1,23 +1,25 @@
 class Demo {
 	public static void main(String... args){
 		Boerderij b1 = new Boerderij();
-		System.out.println(b1.boer.voornaam);
-		b1.lopen(new Boer("ronald"));
-		System.out.println(b1.boer.voornaam);
-		System.out.println("einde");
+		System.out.println(   b1.farmer.voornaam    );
+		Boer boer2 = new Boer("Karel");
+		b1.farmer = boer2;
+		System.out.println(   b1.farmer.voornaam    );
+		Boer farmereur3 = new Boer("Maria");
+		b1.andereBoerAannemen( new Boer("willem") );
+		System.out.println(   b1.farmer.voornaam    );
+		b1.andereBoerAannemen(farmereur3);
 	}	
 }
 class Boerderij{
-	Boer boer = new Boer("joost");	
-	void lopen(Boer boer) {
-		System.out.println(boer.voornaam);
-		this.boer = boer;
+	Boer farmer = new Boer("joost");
+	void andereBoerAannemen(Boer bbb) {
+		this.farmer = bbb;
 	}
 }
 class Boer{
-	String voornaam = "backupvoornaam";
-	Boer(String voornaam){ 
-		System.out.println(voornaam + "is geboren");
-		this.voornaam = voornaam;
+	String voornaam = "frits";
+	Boer(String name){
+		voornaam = name;
 	}
 }
