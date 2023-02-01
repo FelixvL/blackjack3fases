23dad807e8b77b7415b0768bd65b82f120ffa30f
FelixvL <digitaleservice@gmail.com>
2023-02-01 14:42:10 +0100

    zonder accolades 2

--- Demo.java
+++ Demo.java
@@ -3,9 +3,16 @@
 class Demo {
 	public static void main(String... args){
 		int getal = 18;
-		
-		if(getal == 22)
-			System.out.println("hoi");
-			System.out.println("hoi");
+		int tussen = getal < 16 ? 24 : 29;
+					//30
+		if (tussen > 30)//false
+			System.out.println("much");
+			System.out.println("little");
+			System.out.println("less");
+		if (tussen < 30)//true
+			System.out.println("hcum");
+			else
+				System.out.println("elttil");
+				System.out.println("ssel");
 	}	
 }
